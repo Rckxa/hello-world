cda709be5b5b6100dc8c9ff4d277920f6af46de6
Rckxa <chandack24@gmail.com>
2024-01-05 04:52:33 +0300

    merged

--- guessit.py
+++ guessit.py
@@ -1,9 +1,12 @@
 import random
 
+def check(user_guess, correct_number):
+    user_guess = int(input())
+    if(user_guess < correct_number):
+        print('low')
+    elif(user_guess > correct_number):
+        print('high')
+    else:
+        print('correct')
 def gen():
-    #generate a random number
     return random.randint(1, 10)
-
-# our variables
-correct_number = gen();
-user_guess = int(input("Enter a number \n"))
