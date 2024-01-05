a55366ae90ea619edb1185891f955979bfbdd9cb
Nelianjov <phirineliyamercy@gmail.com>
2024-01-05 05:33:11 +0300

    guessit

--- guessit.py
+++ guessit.py
@@ -1,8 +1,16 @@
-def check(int user_guess, int correct_number)
-    user_guess = int(input())
+import random
+def gen():
+    return random.randint(1, 10)
+correct_number = gen()
+user_guess = int(input())
+def check(user_guess,correct_number):
     if(user_guess < correct_number):
         print('low')
     elif(user_guess > correct_number):
         print('high')
     else:
         print('correct')
+while(user_guess != correct_number):
+    user_guess = int(input())
+    check(user_guess,correct_number)
+    
