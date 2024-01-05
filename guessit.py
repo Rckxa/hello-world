71e6a7d9022350a93699721be65e05fa1413959b
Nelianjov <phirineliyamercy@gmail.com>
2024-01-05 04:34:41 +0300

    check(user_guess,correct_number)

--- guessit.py
+++ guessit.py
@@ -1,4 +1,4 @@
-def check(int user_guess, int correct_number):
+def check(int user_guess, int correct_number)
     user_guess = int(input())
     if(user_guess < correct_number):
         print('low')
