ffcd38b6cd4c2d4e4e7d0ba8230a0405f8fe0c2e
Nelianjov <phirineliyamercy@gmail.com>
2024-01-06 23:25:01 +0300

    Added a comment for the user

--- guessit.py
+++ guessit.py
@@ -15,20 +15,11 @@ def check(guess, correct):
 
 
 correct_number = gen()
+print("Am thinking of a number from 1-10,guess the correct number")
 user_guess = int(input())
 
 while user_guess != correct_number:
     user_guess = int(input())
     check(user_guess, correct_number)
 
-#added a new function
-def do_nothing():
-    return 0
 
-def second_function():
-    return 0 #this function also does nothing but will be merged
-            # to master because what the hell
-
-#new line 1 by his branch
-# comment added by hers branch
-# ok last one
